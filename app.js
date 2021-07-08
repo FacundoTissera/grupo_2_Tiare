800f9c83c24fd85e4f88b72ab0f030b1f3d8ca2d
FacundoTissera <facundotisserasorribas@gmail.com>
2021-07-07 23:43:02 -0300

    actualizacion enlaces

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const publicPath = path.join(__dirname, "./public");
 app.use (express.static(publicPath));
 
 app.get('/', (req, res) =>{
-    res.sendFile(path.join(__dirname, '/views/index.html'));
+    res.sendFile(path.join(__dirname, 'views/index.html'));
 });
-app.get('login', (req, res) =>{
-    res.sendFile(path.join(__dirname,'/views/login.html'));
+app.get('/login', (req, res) =>{
+    res.sendFile(path.join(__dirname,'views/login.html'));
 });
 
 app.listen('3000', ()=>{
